3695a287942a7273da33f37087dd54efa75ab3f9
timkambic <tim.kambic12@gmail.com>
2018-09-20 09:18:08 +0200

    added node template

---
+++ node_template.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+class MyClass{
+    ros::Subscriber data_sub;
+    ros::Publisher data_pub;
+    double parameter1;
+
+public:
+    MyClass(){
+        ros::NodeHandle n;
+        ros::NodeHandle np("~");
+        np.getParam("parameter1", parameter1);
+        data_pub = n.advertise<std_msgs::String>("data_out",10);
+        data_sub = n.subscribe("data",10,&MyClass::dataCB,this);
+    }
+
+private:
+    dataCB(const std_msgs::String::ConstPtr &msg){
+        data_pub.publish(msg);
+    }
+};
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "node_name");
+	ros::spin();
+    return 0;
+}
